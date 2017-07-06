650c12c11a98604a618f96e4219cb2212d93a6e7
DantElemer <farkasdomonkosl@gmail.com>
2017-07-06 15:57:31 +0200

    Settings, Game update
    Settings: time of turns slider added (its possible values can be added
    in code, but it doesn't saves anything yet).
    
    Game: main parts added (as empty objects).

--- Ajadeba/Assets/Scripts/Settings.cs
+++ Ajadeba/Assets/Scripts/Settings.cs
@@ -1,18 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
-public class Settings : MonoBehaviour {
+public class Settings : MonoBehaviour 
+{
+	public Slider sldrTimeOfTurn;
+	public Text txtTimeOfTurn;
 
-//	// Use this for initialization
-//	void Start () {
-//	
-//	}
-//	
-//	// Update is called once per frame
-//	void Update () {
-//	
-//	}
+	List<string> turnTimeOps = new List <string>();
+
+	void Start () 
+	{
+		generateTurnTimeOps ();
+		sldrTimeOfTurn.maxValue = turnTimeOps.Count-1;
+	}
+
+	void generateTurnTimeOps()
+	{
+		turnTimeOps.Add ("10 seconds");
+		turnTimeOps.Add ("30 seconds");
+		turnTimeOps.Add ("1 minute");
+		turnTimeOps.Add ("2 minute");
+		turnTimeOps.Add ("infinite");
+	}
+
+
+	public void ChangeTurnTime()
+	{
+		txtTimeOfTurn.text = "Time of turn: " + turnTimeOps [(int)sldrTimeOfTurn.value];
+	}
 
 	public void MainMenu()
 	{
