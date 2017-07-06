f70fb0efdc4d0e9a5429dc76267895bb6a2b9f80
DantElemer <farkasdomonkosl@gmail.com>
2017-07-06 10:15:35 +0200

    Unity project, menu system added I
    MainMenu, Tutorial, Settings, Game sceenes.

---
+++ Ajadeba/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+//	void Start () 
+//	{
+//	
+//	}
+//
+//	void Update ()
+//	{
+//	
+//	}
+
+	public void Tutorial()
+	{
+		SceneManager.LoadScene("Tutorial");
+	}
+
+	public void Play() //starts settings 
+	{
+		SceneManager.LoadScene("Settings");
+	}
+
+	public void Exit()
+	{
+		Application.Quit ();
+	}
+}
---
+++ Ajadeba/Assets/Scripts/Settings.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Settings : MonoBehaviour {
+
+//	// Use this for initialization
+//	void Start () {
+//	
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//	
+//	}
+
+	public void MainMenu()
+	{
+		SceneManager.LoadScene("MainMenu");
+	}
+
+	public void StartGame()
+	{
+		SceneManager.LoadScene("Game");
+	}
+}
