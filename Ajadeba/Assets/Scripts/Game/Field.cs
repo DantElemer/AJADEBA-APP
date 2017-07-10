142e2621d93b08e9ffb7d3addc530987b04e4c2c
DantElemer <farkasdomonkosl@gmail.com>
2017-07-10 20:42:03 +0200

    Map bases
    Blank fields are generated in a square. Pressing the mouse on one causes
    a Debug.Log message containing the field's position (it's a test).

---
+++ Ajadeba/Assets/Scripts/Game/Field.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Field : MonoBehaviour {
+
+	public int index = -1; 
+	public const float WIDTH = 0.3f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown()
+	{
+		MapHandler.FieldPressed (index);
+		//Debug.Log("Hi");
+	}
+}
---
+++ Ajadeba/Assets/Scripts/Game/MapHandler.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapHandler : MonoBehaviour { //TODO add to Map, not to GameHandler?
+
+	public GameObject map;
+	public Field fieldPrefab;
+	public static Field[] fields; //TODO destaticalizálás esetleg
+
+	void Start () {
+		//TODO: generate map
+		GenerateMap();
+	}
+
+	void Update () {
+	
+	}
+
+	void GenerateMap()
+	{
+		fields = new Field[100];
+		for (int i = 0; i < 10; i++)
+			for (int j = 0; j < 10; j++) 
+			{
+				fields [10*i+j] = Instantiate(fieldPrefab);
+				fields [10*i+j].index = 10*i+j;
+				fields [10 * i + j].transform.Translate(new Vector3 (Field.WIDTH * i, Field.WIDTH * j, 0));
+				fields [10 * i + j].transform.SetParent (map.transform);
+			}
+	}
+
+	public static void FieldPressed(int fieldInd) //mouse down on field
+	{
+		Debug.Log (fields[fieldInd].transform.position);
+		//TODO valami értelmeset csinálni itt...
+	}
+}
