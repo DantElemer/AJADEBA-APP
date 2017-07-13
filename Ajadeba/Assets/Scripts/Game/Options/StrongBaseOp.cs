511ffcfbcd577d8626ace5e186df5161186dbaa9
DantElemer <farkasdomonkosl@gmail.com>
2017-07-13 21:34:32 +0200

    Build, yes one more...
    Stronghold bases cant be built in territory.

--- Ajadeba/Assets/Scripts/Game/Options/StrongBaseOp.cs
+++ Ajadeba/Assets/Scripts/Game/Options/StrongBaseOp.cs
@@ -8,4 +8,9 @@ public class StrongBaseOp : BuildOption {
 		if (Input.GetMouseButtonUp (0)) 
 			MapHandler.instance.chosenField.addStrongBase ();
 	}
+
+	protected override bool CanBuild (Field where)
+	{
+		return base.CanBuild(where) && !where.hasOtherOwner(null);
+	}
 }
